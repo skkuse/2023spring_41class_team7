c0804909f9f7af48a4af2b155060e04bb0e9e7cc
xhmxnjxx <ominj98@gmail.com>
2023-05-09 22:01:37 +0900

    input box initial setup

---
+++ frontend/src/components/Input/index.js
@@ -0,0 +1,9 @@
+import { InputBox } from "./style";
+
+function Input(props) {
+    return(
+        <InputBox></InputBox>
+    );
+}
+
+export default Input;
---
+++ frontend/src/components/Input/style.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const InputBox = styled.input`
+    width: 316px;
+    height: 73px;
+    border: 3px solid black;
+    border-radius: 10px;
+`;
