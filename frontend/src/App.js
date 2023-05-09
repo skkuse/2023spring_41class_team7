8a41f8cb0f20278a051674441c80654a8441172e
xhmxnjxx <ominj98@gmail.com>
2023-05-09 22:03:07 +0900

    App.js 원상 복귀

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Input from './components/Input';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <Input></Input>
     </div>
   );
 }
