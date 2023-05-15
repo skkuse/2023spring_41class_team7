69768065aa8d962e27da998676ab0ddb53b40d52
JoYehyun99 <jo990714@naver.com>
2023-05-15 17:47:07 +0900

    nav_component_first

---
+++ frontend/src/components/Navbar/style.js
@@ -0,0 +1,70 @@
+import styled from "@emotion/styled";
+
+export const LetfContainer = styled.div`
+    width: 180px;
+    height: 530px;
+    background: #FFFFFF;
+    box-shadow: 0px 8px 40px rgba(0, 0, 0, 0.09);
+    border-radius: 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 35px;
+`;
+
+export const ProfileContainer = styled.div`
+display: flex;
+width: 100%;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+export const TmpProfile = styled.div`
+width: 120px;
+height: 120px;
+border-radius: 100px;
+background-color: #DFE4E7;
+`;
+
+export const PageBtn = styled.button`
+font-size: 15px;
+padding: 6px 45px;
+letter-spacing: 0.06em;
+background-color: #ffffffff;
+border: 2px solid #F4B860;
+border-radius: 20px;
+cursor: pointer;
+`;
+
+
+export const NavContainer = styled.div`
+display: flex;
+width: 100%;
+height:50%;
+flex-direction: column;
+justfy-content: center;
+align-items: center;
+`;
+
+export const NavTitle = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+font-size: 18px;
+align-items: center;
+margin-bottom: 10px;
+border-bottom: solid 1px #DFE4E7;
+`;
+
+export const NavElement = styled.div`
+width: 60%;
+padding: 10px;
+color: #48413D;
+display: flex;
+align-items: center;
+margin-bottom: 10px;
+gap: 10px;
+`;
