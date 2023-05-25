b8de568856c7a552a783e73b7ffe4af189c52f23
Yoo Young Seok <richpinKR99@gmail.com>
2023-05-25 16:28:22 +0900

    Create Feedback System

---
+++ backend/feedback/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Feedback, Quiz, Analysis
+
+admin.site.register(Feedback)
+admin.site.register(Quiz)
+admin.site.register(Analysis)
+# Register your models here.
---
+++ backend/feedback/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from feedback import views
+
+from django.conf import settings
+
+urlpatterns = [
+    
+]
