280d510124183dd64a488bdce5d50b190e54e87d
Kim, Jung Sik <wjddkel328@gmail.com>
2023-05-25 16:42:22 +0900

    Add learner & modify default permission

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('course/', include('course.urls')),
+    path('learn/', include('learn.urls')),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.response import Response 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from django.contrib.auth import login, logout
 
 from .models import User
@@ -11,35 +9,36 @@ from .serializers import UserSerializer, RegistrationSerializer, LoginSerializer
 # Create your views here.
 
 class UserRegistration(APIView):
+    permission_classes = [AllowAny]
     serializer_class = RegistrationSerializer
 
-    def post(self, request, format=None):
+    def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({ 'user': serializer.data })
+        return Response(serializer.data)
 
 
 class UserLogin(APIView):
+    permission_classes = [AllowAny]
     serializer_class = LoginSerializer
 
-    def post(self, request, format=None):
+    def post(self, request):
         serializer = LoginSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
         login(request, user)
-        return Response({ 'user': UserSerializer(user).data })
+        return Response(UserSerializer(user).data)
     
-    def get(self, request, format=None):
+    def get(self, request):
         logout(request)
         return Response(None, status=status.HTTP_202_ACCEPTED)
 
 
 class Example(APIView):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    
-    def get(self, request, format=None):
+    serializer_class = UserSerializer
+
+    def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
