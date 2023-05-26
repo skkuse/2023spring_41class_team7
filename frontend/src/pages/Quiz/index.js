3df06c6413706274f55a23310b93daaa747fb910
xhmxnjxx <ominj98@gmail.com>
2023-05-26 16:22:56 +0900

    commit

--- frontend/src/pages/Quiz/index.js
+++ frontend/src/pages/Quiz/index.js
@@ -1,26 +1,32 @@
 import Header from "../../components/Header";
-import { CodeEditorContainer, CodeEditorHeader } from "../../components/CodeEditor/style";
+import {
+  CodeEditorContainer,
+  CodeEditorHeader,
+} from "../../components/CodeEditor/style";
 import { CodeEditorCpp } from "../../components/CodeEditor";
-import { QuizOuterContainer, QuizTitleContainer, QuizInnerContainer, QuizUtilContainer, QuizEditorContainer } from "./style";
+import {
+  QuizOuterContainer,
+  QuizTitleContainer,
+  QuizInnerContainer,
+  QuizUtilContainer,
+  QuizEditorContainer,
+} from "./style";
 import SearchHeader from "../../components/SearchHeader";
 
-
-function QuizPage(props){
-    return(
-        <QuizOuterContainer>
-            <Header></Header>
-            <QuizInnerContainer>
-            <QuizUtilContainer>
-                    <QuizTitleContainer></QuizTitleContainer>
-            </QuizUtilContainer>
-            <QuizEditorContainer>
-                    <CodeEditorCpp></CodeEditorCpp>
-            </QuizEditorContainer>
-               
-            </QuizInnerContainer>
-                
-        </QuizOuterContainer>
-    );
+function QuizPage(props) {
+  return (
+    <QuizOuterContainer>
+      <Header></Header>
+      <QuizInnerContainer>
+        <QuizUtilContainer>
+          <QuizTitleContainer></QuizTitleContainer>
+        </QuizUtilContainer>
+        <QuizEditorContainer>
+          <CodeEditorCpp></CodeEditorCpp>
+        </QuizEditorContainer>
+      </QuizInnerContainer>
+    </QuizOuterContainer>
+  );
 }
 
 export default QuizPage;
