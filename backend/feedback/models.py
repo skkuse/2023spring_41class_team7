023a135819698ff53d06a0482febf934cc6cbfae
Yoo Young Seok <richpinKR99@gmail.com>
2023-05-31 17:14:07 +0900

    Get Analysis and Quiz generation

--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -4,9 +4,13 @@ from course.models import Course, Chapter
 # Create your models here.
 
 class Feedback(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
 
 class Quiz(models.Model):
