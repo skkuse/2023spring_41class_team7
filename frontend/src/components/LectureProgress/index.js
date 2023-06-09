b112dfb0eea5d194b8ad9b8fa6728dab8f61127c
kimmstop <kimmstop@gmail.com>
2023-06-09 20:27:52 +0900

    learning page chapter link

--- frontend/src/components/LectureProgress/index.js
+++ frontend/src/components/LectureProgress/index.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import {
   Container,
   ImageContainer,
@@ -7,13 +8,32 @@ import {
   MoreInfoContainer,
   EtcContainer,
 } from "./style";
+import { serverAxios } from "../../utils/commonAxios";
 
 function LectureProgress(props) {
   let course_id = props.data.id;
+  const [lastChapterId, setLastChapterId] = useState("1");
+
+  useEffect(() => {
+    //console.log(courseid);
+    getLastChapterInfo(course_id);
+  }, []);
+
+  const getLastChapterInfo = async (course_id) => {
+    await serverAxios
+      .get(`/learn/course/${course_id}/`, { withCredentials: true })
+      .then((res) => {
+       setLastChapterId(res.data.last_chapter);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  
   const navigate = useNavigate();
   const lectureClick = () => {
-    navigate("/learning/" + course_id);
+    navigate("/learning/" + course_id+ "/" + lastChapterId + "/");
   }
+
   
   //console.log(props.data);
   return (
