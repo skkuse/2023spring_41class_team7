7b5e7e9b0dc17c5f6541ff5f907bfa6cb8b3c206
kimmstop <kimmstop@gmail.com>
2023-06-10 01:17:02 +0900

    kms_merge

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-kmjqc)+k)swb89l7q2_@zsobr3#!hsz9l=g0th+&e*&_^lai&h'
+OPENAI_API_KEY = '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'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
